5358de49f545d427713e13c9309a1f3358fd583f
Chitra-Shrivastav-29 <chitra.2503674201@vcet.edu.in>
2026-08-12 08:39:58 +0530

    Add files via upload

---
+++ bst.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node* insert(Node* root, int val)
+{
+    if(root == NULL)
+        return new Node(val);
+
+    if(val < root->data)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+void inorder(Node* root)
+{
+    if(root == NULL)
+        return;
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void preorder(Node* root)
+{
+    if(root == NULL)
+        return;
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(Node* root)
+{
+    if(root == NULL)
+        return;
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+    Node* root = NULL;
+
+    int arr[] = {50,30,70,20,40,60,80};
+
+    for(int x : arr)
+        root = insert(root, x);
+
+    cout << "Inorder: ";
+    inorder(root);
+
+    cout << "\nPreorder: ";
+    preorder(root);
+
+    cout << "\nPostorder: ";
+    postorder(root);
+
+    return 0;
+} 
